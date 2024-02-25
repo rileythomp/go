e6fb4763a2cae11aaed2edf0fc3f9c3bd79c045e
Riley Thompson <rileythompson99@gmail.com>
2024-02-25 11:45:14 -0500

    ex1

--- ex1.go
+++ ex1.go
@@ -10,5 +10,11 @@ func main() {
 	for i := 8; i <= 20; i += 4 {
 		fmt.Println(i)
 	}
+	var err error = nil
+	unless err == nil {
+		fmt.Println("error")
+	}
+	if err != nil {
+		fmt.Println("error")
+	}
 }
-
